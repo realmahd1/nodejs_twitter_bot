0453cd1b31e366ee144c02e279c217d8713beaa9
M.Mahdi saeidi <m.mahdisaeidi99@gmail.com>
2022-06-13 16:58:12 +0000

    add jokes tweets to bot and change folder structure

---
+++ functions/quotes.js
@@ -0,0 +1,57 @@
+const Twitter = require("twitter")
+const dotenv = require("dotenv")
+const axios = require('axios');
+dotenv.config()
+
+module.exports.quotes = () => {
+
+    const client = new Twitter({
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+        access_token_key: process.env.ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    })
+
+    axios.get('https://type.fit/api/quotes').then((res) => {
+        let englishQuotes = res.data;
+
+        let randomNumber = Math.floor(Math.random() * englishQuotes?.length);
+        function getCharacterLength(str) {
+            return [...str].length;
+        }
+        // handle when quotes length is more than 280 character
+        if (getCharacterLength(englishQuotes[randomNumber].text) < 280) {
+            client.post("statuses/update", { status: `${englishQuotes[randomNumber].text}  \n\n ✍ ${englishQuotes[randomNumber].author} \n\n #quotes #bot` }, function (error, tweet, response) {
+                if (error) {
+                    console.log(error)
+                } else {
+                    console.log(tweet)
+                }
+            })
+        }
+        else {
+            const numberOfTweets = Math.ceil(getCharacterLength(englishQuotes[randomNumber].text) / 280);
+            let firstTweetId;
+
+            client.post("statuses/update", { status: `${englishQuotes[randomNumber].text.substring(0, 280)}` }, function (error, tweet, response) {
+                if (error) {
+                    console.log(error)
+                } else {
+                    firstTweetId = tweet.id_str;
+                    for (let i = 1; i < numberOfTweets; i++) {
+                        let start = 280 * i;
+                        client.post("statuses/update", { in_reply_to_status_id: firstTweetId, status: `${englishQuotes[randomNumber].text.substring(start, start + 280)} ${i === numberOfTweets - 1 ? `\n\n ✍ ${englishQuotes[randomNumber].author} \n\n #quotes #bot` : ''}` }, function (error, tweet, response) {
+                            if (error) {
+                                console.log(error)
+                            } else {
+                                console.log(tweet)
+                            }
+                        })
+                    }
+                }
+            })
+        }
+    }).catch((err) => {
+        console.log(err);
+    })
+}
--- index.js
+++ index.js
@@ -1,52 +1,16 @@
-const Twitter = require("twitter")
-const dotenv = require("dotenv")
-const axios = require('axios');
-dotenv.config()
+const { quotes } = require("./functions/quotes");
+const { jokes } = require("./functions/jokes");
 
-const client = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
-})
+const threeHoursInMiliSeconds = 10800000;
 
-axios.get('https://type.fit/api/quotes').then((res) => {
-  let englishQuotes = res.data;
-
-  let randomNumber = Math.floor(Math.random() * englishQuotes?.length);
-  function getCharacterLength(str) {
-    return [...str].length;
+// run set interval every 3'h if index is even run jokes function and if odd run quotes function
+index = 0;
+setInterval(() => {
+  if (index % 2 === 0) {
+    jokes();
+    index++;
+  } else {
+    quotes();
+    index++;
   }
-  // handle when quotes length is more than 280 character
-  if (getCharacterLength(englishQuotes[randomNumber].text) < 280) {
-    client.post("statuses/update", { status: `${englishQuotes[randomNumber].text}  \n\n ✍ ${englishQuotes[randomNumber].author}` }, function (error, tweet, response) {
-      if (error) {
-        console.log(error)
-      } else {
-        console.log(tweet)
-      }
-    })
-  }
-  else {
-    const numberOfTweets = Math.ceil(getCharacterLength(englishQuotes[randomNumber].text) / 280);
-    let firstTweetId;
-
-    client.post("statuses/update", { status: `${englishQuotes[randomNumber].text.substring(0, 280)}` }, function (error, tweet, response) {
-      if (error) {
-        console.log(error)
-      } else {
-        firstTweetId = tweet.id_str;
-        for (let i = 1; i < numberOfTweets; i++) {
-          let start = 280 * i;
-          client.post("statuses/update", { in_reply_to_status_id: firstTweetId, status: `${englishQuotes[randomNumber].text.substring(start, start + 280)} ${i === numberOfTweets - 1 ? `\n\n ✍ ${englishQuotes[randomNumber].author}` : ''}` }, function (error, tweet, response) {
-            if (error) {
-              console.log(error)
-            } else {
-              console.log(tweet)
-            }
-          })
-        }
-      }
-    })
-  }
-})
+}, threeHoursInMiliSeconds);
