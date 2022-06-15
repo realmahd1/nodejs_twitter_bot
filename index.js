c0170e2596a6eabe23d39c8606dfa1075506508c
M.Mahdi saeidi <m.mahdisaeidi99@gmail.com>
2022-06-15 12:21:36 +0430

    fix mistake

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { jokes } = require("./functions/jokes");
 const threeHoursInMiliSeconds = 10800000;
 
 // run set interval every 3'h if index is even run jokes function and if odd run quotes function
-jokes();
-quotes();
 index = 0;
 setInterval(() => {
   if (index % 2 === 0) {
+    jokes();
     index++;
   } else {
+    quotes();
     index++;
   }
 }, threeHoursInMiliSeconds);
